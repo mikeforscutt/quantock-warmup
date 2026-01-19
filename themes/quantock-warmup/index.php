3d3278fe8997d68c124ee7e9f2de9afb313bed89
mikeforscutt <mikeforscutt@gmail.com>
2026-01-19 15:57:00 +0000

    Add basic loop and front-page scaffold

---
+++ themes/quantock-warmup/index.php
@@ -0,0 +1,14 @@
+<?php get_header(); ?>
+
+<main>
+  <?php if ( have_posts() ) : ?>
+    <?php while ( have_posts() ) : the_post(); ?>
+      <article>
+        <h1><?php the_title(); ?></h1>
+        <?php the_content(); ?>
+      </article>
+    <?php endwhile; ?>
+  <?php endif; ?>
+</main>
+
+<?php get_footer(); ?>
